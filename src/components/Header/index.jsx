d1023d477c1daecc5345def6bbc7f15779fe8fa2
AdilaFrontDev <adila.ambert@hotmail.com>
2024-03-26 09:10:35 +0100

    ajout des links

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/Survey">Questionnaire</Link>
+        </nav>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home/index'
 import Survey from './pages/Survey/Survey'
+import Header from './components/Header/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Survey" element={<Survey />} />
